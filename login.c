0eec84e669833f2f31bbba3e08a7b6f7a132ebc4
hellowo1 <dlwpals0819@naver.com>
2025-11-22 23:47:50 +0900

    간단한 암호화 적용 및 비밀번호 UI 개선

--- login.c
+++ login.c
@@ -9,13 +9,24 @@
 
 #define MAX_USERNAME_LEN 15
 #define MAX_PASSWORD_LEN 15
+
+
 int WIDTH,HEIGHT;
 int loginFlag=0;
 
 typedef struct{
     char id[15];
-    char password[15];
+    unsigned long password;
 }User_Process;
+
+unsigned long hash_password(const char *str) {
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *str++)) {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return hash;
+}
 
 void draw_border(){
     clear();
@@ -36,11 +47,38 @@ void init_screen(){
     curs_set(0);
 }
 
+void get_pw(char *buffer, int y, int x) {
+    int i=0;
+    int ch;
+    move(y,x);
+    while (1) {
+        ch = getch();
+        fflush(stdin);
+        if (ch == '\n' || ch == '\r') { 
+            buffer[i] = '\0';
+            break;
+        } else if (ch == 127 || ch == 8) { 
+            if (i > 0) {
+                i--;
+                mvaddch(y, x + i, ' '); 
+                move(y, x + i);
+            }
+        } else if (i < MAX_PASSWORD_LEN) {
+            buffer[i++] = ch;
+            addch('*'); 
+        }
+        refresh();
+    }
+}
+
+
 void RegisterForm(){
+    char *buffer=malloc(MAX_PASSWORD_LEN+1);
     clear();
     draw_border();
     refresh();
     User_Process t1;
+    mvprintw(HEIGHT/2-2,WIDTH/2-10, "=== REGISTER ===");
     int fid;
     mvprintw(HEIGHT/2,WIDTH/2,"Enter your Id:");
     refresh();
@@ -48,30 +86,25 @@ void RegisterForm(){
     mvgetstr(HEIGHT/2 , WIDTH/2+14, t1.id);
     noecho();
     mvprintw(HEIGHT/2+1,WIDTH/2,"Enter your password:");
-    mvgetstr(HEIGHT/2+1 , WIDTH/2+22, t1.password);
+    get_pw(buffer,HEIGHT/2+1,WIDTH/2+22);
+    echo();
     char buf[100];
-    sprintf(buf,"User: %s\n", t1.id);
+    unsigned long hashed_pw=hash_password(buffer);
+    sprintf(buf,"%s %lu\n", t1.id,hashed_pw);
     if((fid=open("UserLog.txt",O_WRONLY|O_CREAT|O_APPEND,0700))==-1){
         perror("Error!");
         exit(1);
     }
     if (write(fid, buf, strlen(buf)) == -1) {
         endwin();
-        perror("Error writing ID");
-        close(fid);
-        exit(1);
-    }
-
-    sprintf(buf, "Password: %s\n", t1.password);
-    if (write(fid, buf, strlen(buf)) == -1) {
-        endwin();
-        perror("Error writing password");
+        perror("Error writing");
         close(fid);
         exit(1);
     }
     
     close(fid);
     mvprintw(HEIGHT/2+3, (WIDTH-25)/2, "Save Complete. Press any key.");
+    getch();
     refresh();
 }
 
@@ -82,59 +115,39 @@ void loginForm(){
     clear();
     draw_border();
     refresh();
-
+    char *buffer=malloc(MAX_PASSWORD_LEN+1);
     User_Process t1;
+    User_Process che;
     struct stat fst;
     int fid;
-    mvprintw(HEIGHT/2-1,WIDTH/2,"This is loginForm");
+    mvprintw(HEIGHT/2-2,WIDTH/2-10, "=== LOGIN ===");
     mvprintw(HEIGHT/2,WIDTH/2,"Enter your Id:");
     refresh();
     echo(); 
     mvgetstr(HEIGHT/2 , WIDTH/2+14, t1.id);
     noecho();
     mvprintw(HEIGHT/2+1,WIDTH/2,"Enter your password:");
-    mvgetstr(HEIGHT/2+1 , WIDTH/2+22, t1.password);
-    if((fid=open("UserLog.txt",O_RDONLY))==-1){
-        perror("Error!");
-        exit(1);
-    }
-    if(fstat(fid,&fst)==-1){
-        perror("Error!");
-        exit(1);
-    }
-    int lengFile=fst.st_size;
-    char* longBuffer=malloc(lengFile+1);
-    int readLength;
-    if((readLength=read(fid,longBuffer,lengFile))!=lengFile){
-        perror("Error!");
-        free(longBuffer);
-        exit(1);
-    }
-    char* temp=strtok(longBuffer,"\n");
-    while(temp!=NULL){
-        char* user,*passwd,*dummy;
-        dummy = strtok(temp, ":");    
-        user = strtok(NULL, ":");
-        temp = strtok(NULL, "\n");
-        dummy = strtok(temp, ":");  
-        passwd=strtok(NULL,":");
-        if(user!=NULL && passwd!=NULL){
-            // printf("%s,%s",user,passwd);
-            if(strcmp(user,t1.id)==0&&strcmp(passwd,t1.password)==0){
-                loginFlag=1;
-                break;
-            }
-        }
-       
-        temp=strtok(NULL,"\n");
-    }
-    close(fid);
-    free(longBuffer);
+    get_pw(buffer,HEIGHT/2+1,WIDTH/2+22);
+    unsigned long temp_pw=hash_password(buffer);
+    FILE* in=fopen("UserLog.txt","r");
     
-    refresh();
-    if(loginFlag==1){
-        mvprintw(2,5,"Login Sucessful!");
+    while(fscanf(in,"%s %lu",che.id,&che.password)!=-1){
+        if (strcmp(che.id, t1.id) == 0 && temp_pw == che.password) {
+            loginFlag = 1;
+            break;
+        }
     }
+    fclose(in);
+    if(loginFlag){
+        mvprintw(HEIGHT/2+3,(WIDTH-25)/2, "Login Successful!");
+        refresh();
+    }
+    else{
+         mvprintw(HEIGHT/2+3,(WIDTH-25)/2, "Login Failed! Retry");
+        refresh();
+    }
+    
+    getch();
     refresh();
 }
 
